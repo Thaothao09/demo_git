7d6c91a6d422ff590f6cf98037bffc6a19668cf8
Thaothao09 <phuongthaonl.tester@gmail.com>
2025-07-13 18:31:03 +0700

    bt1lv2

--- src/main/java/BTJava/BT3Lv1.java
+++ src/main/java/BTJava/BT3Lv1.java
@@ -1,14 +1,15 @@
 package BTJava;
 import java.util.Scanner;
 public class BT3Lv1 {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập số n: ");
         int n = scanner.nextInt();
         int tong = 0;
-        for (int k = 0; k <= n; k++) {
+        for (int k = 0; k <= n; k+=2) {
             tong += k;
         }
-        System.out.println("Tổng c = " + tong);
+        System.out.println("Tổng số chẵn từ 0 tới n = " + tong);
     }
 }
